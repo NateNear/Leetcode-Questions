3910917459cb6055060823c06efb2c3bfa480c61
Hari <92938055+NateNear@users.noreply.github.com>
2023-09-01 18:08:28 +0530

    Time: 0 ms (100%), Space: 6 MB (11.81%) - LeetHub

--- 1630-count-odd-numbers-in-an-interval-range/1630-count-odd-numbers-in-an-interval-range.cpp
+++ 1630-count-odd-numbers-in-an-interval-range/1630-count-odd-numbers-in-an-interval-range.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     int countOdds(int low, int high) {
-        int count  = 0;
+        int count  = ((high+1)/2) - (low/2);
 
-        while(low<=high){
-            if(low%2 == 0){
-                low++;
-            }
-            else{
-                low++;
-                count++;
-            }
-        }
+        // while(low<=high){
+        //     if(low%2 == 0){
+        //         low++;
+        //     }
+        //     else{
+        //         low++;
+        //         count++;
+        //     }
+        // }
         return count;
     }
 };
