6a38c43b23103b79df913c48345a137bf40d2bba
Hari <92938055+NateNear@users.noreply.github.com>
2023-10-23 00:27:47 +0530

    Time: 7 ms (21.68%), Space: 10 MB (20.37%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int i = 0;
+        while (i < nums.size() && nums[i] < target) {
+            i++;
+        }
+        return i;
+    }
+};
