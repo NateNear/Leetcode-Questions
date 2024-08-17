f1a495a6fb36784cad6953e6cf179cd0f5a1b271
Hari <92938055+NateNear@users.noreply.github.com>
2024-08-17 23:24:42 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (28.40%) - LeetSync

---
+++ 50-powx-n/powx-n.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        double ans = 1.0;
+        long long nn = n;
+
+        if (nn < 0) nn = -nn;
+
+        while (nn > 0) {
+            if (nn % 2 == 1) {
+                ans *= x;
+                nn--;
+            } else {
+                x *= x;
+                nn /= 2;
+            }
+        }
+
+        if (n < 0) ans = 1.0 / ans;
+
+        return ans;
+    }
+
+};
