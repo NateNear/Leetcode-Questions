6e77448d96ef802caf39533494defa7f487b804a
Hari <92938055+NateNear@users.noreply.github.com>
2024-08-18 20:31:44 +0530

    Time: 12 ms (89.06%) | Memory: 26.9 MB (62.48%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -4,12 +4,13 @@ public:
         int cnt = 0;
         int el = 0;
         int n = nums.size();
-        
+
         for(int i=0; i<n; i++){
             if(cnt == 0){
                 el = nums[i];
+                cnt++;
             }
-            if(nums[i] == el){
+            else if(nums[i] == el){
                 cnt++;
             }
             else{
