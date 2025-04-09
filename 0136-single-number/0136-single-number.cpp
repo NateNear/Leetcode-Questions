891a6328348574deaf2663b808dd52be56c188de
Hari <92938055+NateNear@users.noreply.github.com>
2025-04-10 00:55:32 +0530

    Time: 0 ms (100%), Space: 20.5 MB (98.1%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -3,13 +3,8 @@ public:
     int singleNumber(vector<int>& nums) {
         int n = nums.size();
         int ans = 0;
-        sort(nums.begin(), nums.end());
-        for(int i = 0; i < n; i++) {
-            if (i == n - 1 || nums[i] != nums[i + 1]) {
-                ans = nums[i];
-                break;
-            }
-            i++;
+        for(int i=0; i<n; i++){
+            ans = ans^nums[i];
         }
         return ans;
     }
