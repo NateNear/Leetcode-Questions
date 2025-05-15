e5ed11699e3a4ebec72c46677831ad607bc228c3
Hari <92938055+NateNear@users.noreply.github.com>
2025-05-16 01:22:50 +0530

    Time: 0 ms (100%), Space: 31.3 MB (45.82%) - LeetHub

--- 0792-binary-search/0792-binary-search.cpp
+++ 0792-binary-search/0792-binary-search.cpp
@@ -1,12 +1,22 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-        int n = nums.size();
 
-        for(int i=0; i<n; i++){
-            if(nums[i] == target)
-            return i;
-        }
-        return -1;
+    int recBS(vector<int>& nums, int s, int e, int target) {
+        if (s > e)
+            return -1;
+
+        int mid = s + (e - s) / 2;
+
+        if (nums[mid] == target)
+            return mid;
+
+        if (nums[mid] < target)
+            return recBS(nums, mid + 1, e, target);
+        else
+            return recBS(nums, s, mid - 1, target);
+    }
+
+    int search(vector<int>& nums, int target) {
+        return recBS(nums, 0, nums.size() - 1, target);
     }
 };
